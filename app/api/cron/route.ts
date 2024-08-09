50dc6693698a93086a5408c93fbb24cf03715746
mirojjj <raimiroj7@gmail.com>
2024-08-09 22:40:33 +0545

    changes

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 60; 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
@@ -14,7 +14,7 @@ export async function GET(request: Request) {
   try {
     connectToDB();
 
-    const products = await Product.findById({});
+    const products = await Product.find({});
 
     if (!products) throw new Error("No product fetched");
 
