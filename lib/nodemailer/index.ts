b358aec22f5ea55b6f4af33a23e44b6356a29ae3
mirojjj <raimiroj7@gmail.com>
2024-08-09 23:41:01 +0545

    ok

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -99,9 +99,20 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
     subject: emailContent.subject,
   }
 
-  await transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if(error) return console.log(error);
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+
+  // await transporter.sendMail(mailOptions, (error: any, info: any) => {
+  //   if(error) return console.log(error);
     
-    console.log('Email sent: ', info);
-  })
+  //   console.log('Email sent: ', info);
+  // })
 }
